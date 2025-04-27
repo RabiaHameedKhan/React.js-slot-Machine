59908791f603ead0f54ba539f436d1fbc01438f1
RabiaHameedKhan <rabiahameedkhan27@gmail.com>
2025-04-27 19:17:25 +0500

    iniial

--- src/App.js
+++ src/App.js
@@ -1,5 +1,43 @@
 import React from 'react';
 
-const App =()=>{};
+// Slot Component
+const Slot = (props) => {
+  let x = props.x;
+  let y = props.y;
+  let z = props.z;
 
-export default app;
+  if (x === y && y === z) {
+    return (
+      <div className="slot_inner">
+        <h1>{x} {y} {z}</h1>
+        <p>This is matching 🎉</p>
+      </div>
+    );
+  } else {
+    return (
+      <div className="slot_inner">
+        <h1>{x} {y} {z}</h1>
+        <p>This is not matching ❌</p>
+      </div>
+    );
+  }
+};
+
+// App Component
+const App = () => {
+  return (
+    <>
+      <h1> 🎰 Welcome to <span>Slot Machine Game 🎰</span> </h1>
+
+      <Slot x="😄" y="😄" z="😄" />
+      <hr />
+      <Slot x="😃" y="😄" z="😄" />
+      <hr />
+      <Slot x="😄" y="🍎" z="😄" />
+      <hr />
+      <Slot x="👍" y="🍎" z="😄" />
+    </>
+  );
+};
+
+export default App;
